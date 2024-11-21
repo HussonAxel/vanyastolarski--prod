0c9a156e5f673b2e73e6ea8361b3e99f24ca66ae
axel husson <aaaxel.hsson@gmail.com>
2024-11-21 15:24:54 +0100

    fix pictures issues and add meta tags for seo on index

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -3,6 +3,7 @@ import LatestRelease from "@components/LatestRelease/LatestRelease.tsx";
 import BookResume from "@components/BookResume/BookResume";
 import LatestReading from '@components/LatestReading/LatestReading';
 import ServicesList from '@components/ServicesList/ServicesList';
+import { Helmet } from 'react-helmet-async';
 
 import ColoredSection from "@components/ColoredSection.tsx";
 import data from "@data/data.json";
@@ -12,24 +13,56 @@ export const Route = createLazyFileRoute('/')({
 })
 
 const IndexComponent = () => {
-    return (
-      <>
-        <ColoredSection bgColor="bg-white">
-          <LatestRelease
-            LatestReleaseName="Fous Papillons"
-            LatestReleaseSynopsis={data.books[2].synopsis}
-            youtubeEmbedUrl="https://www.youtube.com/embed/oMXpFLSyzow"
-            latestReleasePageUrl="livres/2" latestReleasePictureUrl={''}          />
-        </ColoredSection>
-        <ColoredSection bgColor="bg-green">
-          <BookResume />
-        </ColoredSection>
-        <ColoredSection bgColor="bg-white">
-          <LatestReading />
-        </ColoredSection>
-        <ColoredSection bgColor="bg-green">
-          <ServicesList />
-        </ColoredSection>
-      </>
-    );
-}
+  return (
+    <>
+      <Helmet>
+        <title>Vanya Stolarski | Auteure de romans</title>
+        <meta
+          name="description"
+          content="Découvrez l'univers littéraire de Vanya Stolarski, auteure de romans contemporains. Explorez ses œuvres, son dernier livre 'Fous Papillons' et ses actualités."
+        />
+
+        <meta property="og:title" content="Vanya Stolarski | Romancière" />
+        <meta
+          property="og:description"
+          content="Plongez dans l'univers de Vanya Stolarski, auteure de romans qui explore [thèmes principaux]. Découvrez ses œuvres et son actualité littéraire."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="[URL-photo-auteur]" />
+        <meta property="og:url" content="[URL-site]" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Vanya Stolarski | Écrivaine" />
+        <meta
+          name="twitter:description"
+          content="Romans, actualités et rencontres littéraires de Vanya Stolarski, auteure contemporaine."
+        />
+
+        <meta
+          name="keywords"
+          content="Vanya Stolarski, roman contemporain, littérature française, Fous Papillons, auteure, écrivaine, romans"
+        />
+        <meta name="author" content="Vanya Stolarski" />
+        <link rel="canonical" href="[URL-site]" />
+      </Helmet>
+      <ColoredSection bgColor="bg-white">
+        <LatestRelease
+          LatestReleaseName="Fous Papillons"
+          LatestReleaseSynopsis={data.books[2].synopsis}
+          youtubeEmbedUrl="https://www.youtube.com/embed/oMXpFLSyzow"
+          latestReleasePageUrl="livres/2"
+          latestReleasePictureUrl={""}
+        />
+      </ColoredSection>
+      <ColoredSection bgColor="bg-green">
+        <BookResume />
+      </ColoredSection>
+      <ColoredSection bgColor="bg-white">
+        <LatestReading />
+      </ColoredSection>
+      <ColoredSection bgColor="bg-green">
+        <ServicesList />
+      </ColoredSection>
+    </>
+  );
+};
