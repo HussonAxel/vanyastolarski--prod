6b7215ffe6ca4d404109797421c1850c6f9f4adb
axel husson <aaaxel.hsson@gmail.com>
2024-11-22 22:14:49 +0100

    change some typo and favicon

--- src/components/CitationModel/CitationModel.tsx
+++ src/components/CitationModel/CitationModel.tsx
@@ -8,12 +8,12 @@ Thoughts}) => {
   return (
     <article className="font-WorkSans border-b-[0.5px] first-of-type:border-t-[0.5px] flex flex-col sm:flex-row w-3/4 m-auto justify-between py-12">
       <div className=" text-black/80 mb-8">
-        {quoteNumber} - {CreatedAt}
+        {quoteNumber} — {CreatedAt}
       </div>
       <div className="w-full md:max-w-3/4 md:max-w-[800px]">
         <div className="mb-12 gap-4 flex flex-col">
           <h2 className="font-semibold text-2xl">"{Quote}"</h2>
-          <p className="text-lg italic"> - {AuthorOfQuote}</p>
+          <p className="text-lg italic"> — {AuthorOfQuote}</p>
         </div>
         <div>
           <p className="text-black/80">{Thoughts}h</p>
