e3d8d0f0a3df1f922682d7fd2b6cbfe644cac6e9
axel husson <aaaxel.hsson@gmail.com>
2024-12-19 20:06:50 +0100

    fix height size on mobile

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,20 +4,22 @@ import {
   ScrollRestoration,
 } from "@tanstack/react-router";
 import "../index.css";
-
 import Header from "@components/Header/Header";
 import Footer from "@components/Footer/Footer";
+
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <div className="min-h-screen flex flex-col bg-white">
       <ScrollRestoration />
-      <div className="bg-white px-0 lg:px-32">
+      <div className="px-0 lg:px-32">
         <Header />
       </div>
-      <Outlet />
-      <div className="bg-white px-0 lg:px-32">
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <div className="px-0 lg:px-32">
         <Footer />
       </div>
-    </>
+    </div>
   ),
 });
